aa6139bcdd2104977e67f5daa04ff4a556a9b801
jontofront <jontofront@gmail.com>
2024-10-08 09:06:56 +0300

    move code

--- custom_components/econet300/number.py
+++ custom_components/econet300/number.py
@@ -60,11 +60,11 @@ class EconetNumber(EconetEntity, NumberEntity):
         _LOGGER.debug("EconetNumber _sync_state: %s", value)
         self._attr_native_value = value
         map_key = NUMBER_MAP.get(self.entity_description.key)
+        self._attr_native_min_value = ENTITY_MIN_VALUE.get(map_key)
+        self._attr_native_max_value = ENTITY_MAX_VALUE.get(map_key)
         self.async_write_ha_state()
         self.hass.async_create_task(self.async_set_limits_values())
-ntity_description.key)
-        self._attr_native_min_value = ENTITY_MIN_VALUE.get(map_key)
-        self._attr_native_max_value 
+
     async def async_set_limits_values(self):
         """Async Sync number limits."""
         limits = await self.api.get_param_limits(self.entity_description.key)
