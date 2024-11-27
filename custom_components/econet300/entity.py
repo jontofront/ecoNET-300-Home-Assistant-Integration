f4bd77334cfa7d50b4c3e05c1da818917ba9d315
jontofront <jontofront@gmail.com>
2024-11-27 14:46:12 +0200

    revert another funkcion

--- custom_components/econet300/entity.py
+++ custom_components/econet300/entity.py
@@ -1,7 +1,6 @@
 """Base econet entity class."""
 
 import logging
-from typing import Any
 
 from homeassistant.core import callback
 from homeassistant.helpers.entity import DeviceInfo, EntityDescription
@@ -34,11 +33,6 @@ class EconetEntity(CoordinatorEntity):
     @property
     def unique_id(self) -> str | None:
         """Return the unique_id of the entity."""
-        if not self.api.uid:
-            _LOGGER.error(
-                "API UID is missing for entity: %s", self.entity_description.key
-            )
-            return None
         return f"{self.api.uid}-{self.entity_description.key}"
 
     @property
@@ -55,16 +49,6 @@ class EconetEntity(CoordinatorEntity):
             hw_version=self.api.hw_ver,
         )
 
-    def _get_value(self, expected_key: str) -> Any:
-        """Retrieve the value for the expected key from sysParams or regParams."""
-        sys_params = self.coordinator.data.get("sysParams", {})
-        reg_params = self.coordinator.data.get("regParams", {})
-        if expected_key in sys_params:
-            return sys_params[expected_key]
-        if expected_key in reg_params:
-            return reg_params[expected_key]
-        return None
-
     @callback
     def _handle_coordinator_update(self) -> None:
         """Handle updated data from the coordinator."""
@@ -72,7 +56,7 @@ class EconetEntity(CoordinatorEntity):
             "Update EconetEntity, entity name: %s", self.entity_description.name
         )
 
-        value = self._get_value(self.entity_description.key)
+        value = self.coordinator.data["sysParams"].get(self.entity_description.key)
         if value is None:
             _LOGGER.debug("Value for key %s is None", self.entity_description.key)
             return
@@ -84,29 +68,47 @@ class EconetEntity(CoordinatorEntity):
         _LOGGER.debug("Added to HASS: %s", self.entity_description)
         _LOGGER.debug("Coordinator: %s", self.coordinator)
 
+        # Retrieve sysParams and regParams data
+        sys_params = self.coordinator.data.get("sysParams", {})
+        reg_params = self.coordinator.data.get("regParams", {})
+        _LOGGER.debug("sysParams: %s", sys_params)
+        _LOGGER.debug("regParams: %s", reg_params)
+
         # Check if the coordinator has a 'data' attributes
         if "data" not in dir(self.coordinator):
             _LOGGER.error("Coordinator object does not have a 'data' attribute")
             return
 
-        # # Check the expected key and retrieve the value entity_description
+        # Check the available keys in both sourcese
+        sys_keys = sys_params.keys()
+        reg_keys = reg_params.keys()
+        _LOGGER.debug("Available keys in sysParams: %s", sys_keys)
+        _LOGGER.debug("Available keys in regParams: %s", reg_keys)
+
+        # Expected key from entity_description
         expected_key = self.entity_description.key
         _LOGGER.debug("Expected key: %s", expected_key)
 
         # Retrieve the value from sysParams or regParams
-        value = self._get_value(expected_key)
-        if value is None:
-            sys_keys = self.coordinator.data.get("sysParams", {}).keys()
-            reg_keys = self.coordinator.data.get("regParams", {}).keys()
+        value = None
+        if expected_key in sys_params:
+            value = sys_params[expected_key]
+            _LOGGER.debug(
+                "Found key '%s' in sysParams with value: %s", expected_key, value
+            )
+        elif expected_key in reg_params:
+            value = reg_params[expected_key]
+            _LOGGER.debug(
+                "Found key '%s' in regParams with value: %s", expected_key, value
+            )
+        else:
             _LOGGER.warning(
                 "Data key: %s was expected to exist but it doesn't. Available sysParams keys: %s, regParams keys: %s",
                 expected_key,
-                list(sys_keys),
-                list(reg_keys),
+                sys_keys,
+                reg_keys,
             )
             return
-
-        _LOGGER.debug("Retrieved value for key %s: %s", expected_key, value)
 
         # Synchronize with HASS
         await super().async_added_to_hass()
