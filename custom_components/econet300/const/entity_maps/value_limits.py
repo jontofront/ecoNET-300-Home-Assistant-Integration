4b1d92ea26b3e09dac9f4d691dac9704f84b5f22
Jon <jontofront@gmail.com>
2026-06-29 23:44:07 +0300

    refactor: reorganize entity attribute mappings into dedicated modules for improved maintainability and clarity

---
+++ custom_components/econet300/const/entity_maps/value_limits.py
@@ -0,0 +1,16 @@
+"""Entity value limits (min, max, step) for number entities."""
+
+ENTITY_MIN_VALUE = {
+    "tempCOSet": 27,
+    "tempCWUSet": 20,
+}
+
+ENTITY_MAX_VALUE = {
+    "tempCOSet": 68,
+    "tempCWUSet": 55,
+}
+
+ENTITY_STEP = {
+    "tempCOSet": 1,
+    "tempCWUSet": 1,
+}
